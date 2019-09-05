d37666cc10b627c204b7a4d0ad309520d3a3bd29
Student <example@example.com>
2019-09-05 15:56:15 -0700

    initial

---
+++ src/main/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import art from './../img/10.jpg';
+import css from './main.css'
+class Main extends React.Component {
+  render () {
+    return (
+      <div className='background'>
+      <div className='main-container'>I AM <br></br>AMAZING
+      </div>
+      </div>
+    );
+  }
+}
+export default Main;
